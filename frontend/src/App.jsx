0d565ae882ac48d52955f6673227445e8a2a1e20
Lokitroki <159892331+Lokitroki@users.noreply.github.com>
2024-06-24 09:35:40 +0200

    Frontend & routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import NavBar from './components/NavBar'
+import CreateCourse from './components/CreateCourse'
+import CourseList from './components/CourseList'
+import CoursePurchased from './components/CoursePurchased'
+import CreateUsers from './components/CreateUsers'
 
+
+function App() {
   return (
-    <div>
-      Hellow world
-    </div>
-  )
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<CourseList/>}/>
+        <Route path='/coursePurchased' element={<CoursePurchased/>}/>
+        <Route path='/create' element={<CreateCourse/>}/>
+        <Route path='/edit/:id' element={<CreateCourse/>}/>
+        <Route path='/user' element={<CreateUsers/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
